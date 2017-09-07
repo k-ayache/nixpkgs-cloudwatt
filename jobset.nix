65dc873288a74e183daf3b407710a74863518bf5
lewo <lewo@abesis.fr>
2017-09-07 15:40:12 +0200

    Add hydra image push job

--- jobset.nix
+++ jobset.nix
@@ -1,6 +1,23 @@
+{ nixpkgs
+, registryUrl ? "localhost:5000"
+, registryUsername ? null
+  # The path of a file containing the Docker registry password
+, registryPasswordFilepath ? null,
+}:
+
 let
-  pkgs = import <nixpkgs> {};
+  pkgs = import nixpkgs {};
   lib = import ./lib pkgs;
   ci = import ./ci {inherit pkgs;};
+  # Read the registry password from a local file
+  registryPassword = if (registryPasswordFilepath != null && builtins.pathExists registryPasswordFilepath)
+                     then pkgs.lib.removeSuffix "\n" (builtins.readFile registryPasswordFilepath)
+		     else null;
 in
-  { ci.hydra = lib.dockerImageBuildProduct ci.hydra; }
+  {
+    ci.hydraImage = lib.dockerImageBuildProduct ci.hydra;
+    ci.pushHydraImage = lib.dockerPushImage {
+      image = ci.hydra;
+      url = registryUrl; username = registryUsername; password = registryPassword;
+    };
+  }
--- lib/image.nix
+++ lib/image.nix
@@ -3,6 +3,23 @@ pkgs:
 with import ../deps.nix pkgs;
 
 {
+  dockerPushImage = { image, url ? "localhost:5000", username ? null, password ? null }:
+    let
+      imageRef = "${image.imageName}:${image.imageTag}";
+      destCreds = if (username != null && password != null) then "--dest-creds ${username}:${password}" else null;
+      creds     = if (username != null && password != null) then "--creds ${username}:${password}" else null;
+      jobName = with pkgs.lib; "push-" + (removeSuffix ".tar" (removeSuffix ".gz" image.name));
+    in
+      pkgs.runCommand jobName {
+        buildInputs = with pkgs; [ jq skopeo ];
+      } ''
+      echo "Ungunzip image (since skopeo doesn't support tgz image)..."
+      gzip -d ${image.out} -c > image.tar
+      echo "Pushing unzipped image ${image.out} ($(du -hs image.tar | cut -f1)) to registry ${url}/${imageRef} ..."
+      skopeo --insecure-policy copy ${destCreds} --dest-tls-verify=false --dest-cert-dir=/tmp docker-archive:image.tar docker://${url}/${imageRef} > skipeo.log
+      skopeo --insecure-policy inspect ${creds} --tls-verify=false --cert-dir=/tmp docker://${url}/${imageRef} > $out
+    '';
+
   # We want that Hydra generates a link to manually download the image
   dockerImageBuildProduct = image: pkgs.runCommand "${image.name}" {} ''
     mkdir $out
