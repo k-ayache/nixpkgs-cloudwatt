d0733f055f73f1934a985349b84ef24c31b9f1ef
lewo <lewo@abesis.fr>
2017-09-13 18:26:21 +0200

    Fix test inputs evaluation

--- jobset.nix
+++ jobset.nix
@@ -2,16 +2,17 @@
 # only used to checkout the specified nixpkgs commit.
 { bootstrap_pkgs ? <nixpkgs>
 , fetched ? import ./nixpkgs-fetch.nix { nixpkgs = bootstrap_pkgs; }
-, pkgs ? import (fetched.pkgs) {}
+, _pkgs ? fetched.pkgs
 , contrail ? fetched.contrail
 }:
 
 let
+  pkgs = import _pkgs {};
   lib = import ./lib pkgs;
   ci = import ./ci {inherit pkgs;};
 in
   {
     ci.hydraImage = lib.dockerImageBuildProduct ci.hydra;
     ci.pushHydraImage = lib.dockerPushImage ci.hydra;
-    contrail = import contrail {inherit pkgs; };
+    contrail = import contrail {inherit pkgs;  pkgs_path = _pkgs;};
   }
