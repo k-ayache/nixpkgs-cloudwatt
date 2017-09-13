ed821d8b36b575e99a142056cc36090931b5ae84
lewo <lewo@abesis.fr>
2017-09-13 17:23:37 +0200

    Fix nixpkgs-contrail sha

--- nixpkgs-fetch.nix
+++ nixpkgs-fetch.nix
@@ -5,12 +5,13 @@ in {
   pkgs = bootstrap_pkgs.fetchFromGitHub {
     owner = "nlewo";
     repo = "nixpkgs";
-    # Part of the branch release-17.09-cloudwatt
+    # Belong to the branch release-17.09-cloudwatt
     rev = "0c41433868ad61aac43da184c113f305a3784957";
     sha256 = "0jrlk9wwbskzw2bxvncna1fi4qm596r83smcfh1dashb8gm3ddp8";};
   contrail = bootstrap_pkgs.fetchFromGitHub {
     owner = "nlewo";
     repo = "nixpkgs-contrail";
+    # Belong to the master branch
     rev = "b252f9d1727554dc292b26507a9395f330a1c598";
-    sha256 = "0jrlk9wwbskzw2bxvncna1fi4qm596r83smcfh1dashb8gm3ddp8";};
+    sha256 = "0jhnqr4zagk371z3ddyq8pn5n4fr6cplv60vi53933kjwk45vddp";};
   }
