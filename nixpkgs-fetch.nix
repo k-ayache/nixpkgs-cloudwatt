dd8142bb3f2b663020f725316523b64bfe91d895
Antoine Eiche <lewo@abesis.fr>
2018-01-17 11:07:11 +0100

    Upgrade nixpkgs-17.09

--- nixpkgs-fetch.nix
+++ nixpkgs-fetch.nix
@@ -3,11 +3,11 @@
 let bootstrap_pkgs = import nixpkgs {};
 in {
   pkgs = bootstrap_pkgs.fetchFromGitHub {
-    owner = "nlewo";
-    repo = "nixpkgs";
+    owner = "NixOS";
+    repo = "nixpkgs-channels";
     # Belong to the branch release-17.09-cloudwatt
-    rev = "cb5ac615101c5ef346a88648f918685f39f335d8";
-    sha256 = "0lrn1vvbli82zkcq32dkkj56b1l008bmpw2zyhmsw85ajqw8imab";};
+    rev = "66b4de79e3841530e6d9c6baf98702aa1f7124e4";
+    sha256 = "1l3lwi944hnxka0nfq9a1g86xhc0b8hzqr2fm6cvds33gj26l0g4";};
   contrail = bootstrap_pkgs.fetchFromGitHub {
     owner = "nlewo";
     repo = "nixpkgs-contrail";
