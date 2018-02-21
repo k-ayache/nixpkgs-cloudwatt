61d0f54fc7a597774da0cf4fdc133e3b9d2a930e
Antoine Eiche <lewo@abesis.fr>
2018-02-21 11:40:55 +0100

    ci: add staging jobset

--- ci/jobsets-declarative.nix
+++ ci/jobsets-declarative.nix
@@ -36,6 +36,35 @@ let
         };
       };
     };
+    staging = {
+      description = "Build master of nixpkgs-cloudwatt and nixpkgs-contrail";
+      checkinterval = "60";
+      enabled = "1";
+      nixexprinput = "cloudwatt";
+      nixexprpath = "jobset.nix";
+      schedulingshares = 100;
+      enableemail = false;
+      emailoverride = "";
+      keepnr = 1;
+      hidden = false;
+      inputs = {
+        cloudwatt = {
+          value = "https://git.corp.cloudwatt.com/applications/nixpkgs-cloudwatt master keepDotGit";
+          type = "git";
+          emailresponsible = false;
+        };
+        bootstrap_pkgs = {
+          value = "https://git.corp.cloudwatt.com/applications/nixpkgs acd89daabcb47cb882bc72ffc2d01281ed1fecb8";
+          type = "git";
+          emailresponsible = false;
+        };
+        contrail = {
+          value = "https://github.com/nlewo/nixpkgs-contrail master";
+          type = "git";
+          emailresponsible = false;
+        };
+      };
+    };
   };
 
 in {
