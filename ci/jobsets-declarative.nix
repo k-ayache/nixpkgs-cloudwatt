15067c1f9187abffcd6ff5ed28abc0f756f73593
Antoine Eiche <lewo@abesis.fr>
2018-03-12 16:44:33 +0100

    ci: add testing jobset

--- ci/jobsets-declarative.nix
+++ ci/jobsets-declarative.nix
@@ -65,6 +65,30 @@ let
         };
       };
     };
+    testing = {
+      description = "Build testing branch of nixpkgs-cloudwatt";
+      checkinterval = "60";
+      enabled = "1";
+      nixexprinput = "cloudwatt";
+      nixexprpath = "jobset.nix";
+      schedulingshares = 100;
+      enableemail = false;
+      emailoverride = "";
+      keepnr = 1;
+      hidden = false;
+      inputs = {
+        cloudwatt = {
+          value = "https://github.com/nlewo/nixpkgs-cloudwatt testing keepDotGit";
+          type = "git";
+          emailresponsible = false;
+        };
+        bootstrap_pkgs = {
+          value = "https://github.com/NixOS/nixpkgs acd89daabcb47cb882bc72ffc2d01281ed1fecb8";
+          type = "git";
+          emailresponsible = false;
+        };
+      };
+    };
   };
 
 in {
