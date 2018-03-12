f1d34280994c747601d1bc7c4f82e47713a2ebf9
Antoine Eiche <lewo@abesis.fr>
2018-03-12 17:20:31 +0100

    Update nixpkgs to validate a fix regarding image reproducibility
    We temporary swtich to my github account since these patchs have not
    been backported.

--- nixpkgs-fetch.nix
+++ nixpkgs-fetch.nix
@@ -3,11 +3,11 @@
 let bootstrap_pkgs = import nixpkgs {};
 in {
   pkgs = bootstrap_pkgs.fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs-channels";
+    owner = "nlewo";
+    repo = "nixpkgs";
     # Belong to the branch release-17.09-cloudwatt
-    rev = "66b4de79e3841530e6d9c6baf98702aa1f7124e4";
-    sha256 = "1l3lwi944hnxka0nfq9a1g86xhc0b8hzqr2fm6cvds33gj26l0g4";};
+    rev = "9d080dba21ec31cc824ad896dd66bb1b9c8108ac";
+    sha256 = "14kc5dgjx1mpmwr3ha9gpw24mxk6c80x011q5aj9p6a4clj4h8ac";};
   contrail = bootstrap_pkgs.fetchFromGitHub {
     owner = "nlewo";
     repo = "nixpkgs-contrail";
