7debdfdc8870d080bbd6e175dd38ae96735a44ea
Antoine Eiche <lewo@abesis.fr>
2018-04-25 14:13:43 +0200

    ci: Add testing job on sec CI

--- ci/jobsets-declarative-sec.nix
+++ ci/jobsets-declarative-sec.nix
@@ -51,6 +51,31 @@ let
         };
       };
     };
+
+    testing = {
+      description = "Build testing branch of nixpkgs-cloudwatt";
+      checkinterval = "60";
+      enabled = "1";
+      nixexprinput = "cloudwatt";
+      nixexprpath = "jobset.nix";
+      schedulingshares = 100;
+      enableemail = false;
+      emailoverride = "";
+      keepnr = 1;
+      hidden = false;
+      inputs = {
+        cloudwatt = {
+          value = "https://git.sec.cloudwatt.com/applications/nixpkgs-cloudwatt testing keepDotGit";
+          type = "git";
+          emailresponsible = false;
+        };
+        bootstrap_pkgs = {
+          value = "https://git.sec.cloudwatt.com/applications/nixpkgs acd89daabcb47cb882bc72ffc2d01281ed1fecb8";
+          type = "git";
+          emailresponsible = false;
+        };
+      };
+    };
   };
 
 in {
