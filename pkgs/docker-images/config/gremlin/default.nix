570dd5d239026f59ab4baacc9dc99340a1c15dc5
Jean-Philippe Braun <eon@patapon.info>
2018-07-02 15:02:09 +0200

    dockerImages.gremlinServer: filter out deleted vertices in n graph

--- pkgs/docker-images/config/gremlin/default.nix
+++ pkgs/docker-images/config/gremlin/default.nix
@@ -61,7 +61,7 @@ rec {
     text = ''
       def globals = [:]
 
-      globals << [g : graph.traversal(), n : graph.traversal().withStrategies(SubgraphStrategy.build().vertices(hasNot('_missing').hasNot('_incomplete')).create())]
+      globals << [g : graph.traversal(), n : graph.traversal().withStrategies(SubgraphStrategy.build().vertices(hasNot('_missing').hasNot('_incomplete').has('deleted', 0)).create())]
     '';
   };
 
