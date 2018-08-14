cb5df25cf6cb053a36434194dcb8c92c60de5bad
Antoine Eiche <lewo@abesis.fr>
2018-08-14 15:32:07 +0200

    dockerImages.gremlin*: moving from default.nix to a dedicated directory

--- pkgs/docker-images/default.nix
+++ pkgs/docker-images/default.nix
@@ -1,73 +1,14 @@
 { callPackage, callPackages, pkgs, lib, contrail32Cw, locksmith, contrailPath, nixpkgs, waitFor, fluentdCw }:
 
-let
-
-  config = {
-    gremlin = import ./config/gremlin { inherit pkgs contrail32Cw; };
-  };
-
-in
-
 callPackages ./contrail { inherit contrailPath; } //
 {
   hydra = callPackage ./hydra { };
 
   locksmithWorker = callPackage ./locksmithWorker { };
 
-  gremlinServer = lib.buildImageWithPerps {
-    name = "gremlin/server";
-    fromImage = lib.images.kubernetesBaseImage;
-    services = [
-      {
-        name = "gremlin-server";
-        preStartScript = config.gremlin.serverPreStart;
-        chdir = "${contrail32Cw.tools.gremlinServer}/opt";
-        command = "${contrail32Cw.tools.gremlinServer}/bin/gremlin-server ${config.gremlin.serverConf}";
-        fluentd = {
-          source = {
-            type = "stdout";
-            time_format = "%H:%M:%S.%L";
-            format = ''/^(?<time>[^ ]+) (?<classname>[^ ]+) \[(?<level>[^\]]+)\] (?<message>.*)$/'';
-          };
-        };
-      }
-      {
-        name = "gremlin-sync";
-        preStartScript = config.gremlin.syncPreStart;
-        environmentFile = "/run/consul-template-wrapper/env";
-        command = "${contrail32Cw.tools.contrailGremlin}/bin/gremlin-sync";
-        fluentd = {
-          source = {
-            type = "stdout";
-            time_format = "%H:%M:%S.%L";
-            format = ''/^(?<time>[^ ]+) (?<funcname>[^ ]+) \[(?<level>[^\]]+)\] (?<message>.*)$/'';
-          };
-        };
-      }
-    ];
-    contents = [
-      contrail32Cw.tools.contrailGremlin
-    ];
-  };
+  gremlinServer = callPackage ./gremlin-server { };
 
-  gremlinFsck = lib.buildImageWithPerps {
-    name = "gremlin/fsck";
-    fromImage = lib.images.kubernetesBaseImage;
-    services = [
-      {
-        name = "gremlin-fsck";
-        preStartScript = config.gremlin.fsckPreStart;
-        environmentFile = "/run/consul-template-wrapper/env";
-        command = "${contrail32Cw.tools.contrailApiCliWithExtra}/bin/contrail-api-cli fsck";
-        fluentd = {
-          source = {
-            type = "stdout";
-            format = "json";
-          };
-        };
-      }
-    ];
-  };
+  gremlinFsck = callPackage ./gremlin-fsck { };
 
   skydiveAnalyzer = callPackage ./skydive { };
 }
---
+++ pkgs/docker-images/gremlin-fsck/default.nix
@@ -0,0 +1,49 @@
+{ lib, contrail32Cw, writeTextFile }:
+
+let
+  fsckEnv = writeTextFile {
+    name = "env.ctmpl";
+    text = ''
+      {{ $openstack_region := env "openstack_region" -}}
+      {{ $catalog := key (printf "/config/openstack/catalog/%s/data" $openstack_region) | parseJSON -}}
+      OS_AUTH_URL={{ $catalog.identity.internal_url }}
+      OS_USERNAME=deployment
+      OS_TENANT_NAME=deployment
+      {{ with secret "secret/openstack/users/deployment" -}}
+      OS_PASSWORD={{ .Data.password }}
+      {{- end }}
+      OS_ENDPOINT_TYPE=internalURL
+      OS_AUTH_PLUGIN=v2password
+
+      CONTRAIL_API_HOST=contrail-api
+      CONTRAIL_API_CLI_CONFIG_DIR=/tmp
+
+      GREMLIN_FSCK_SERVER=gremlin-server-pods.service:8182
+      GREMLIN_FSCK_LOOP=1
+      GREMLIN_FSCK_JSON=1
+      GREMLIN_FSCK_ZK_SERVER=opencontrail-config-zookeeper.service:2181
+    '';
+  };
+
+  fsckPreStart = ''
+    consul-template-wrapper -- -once -template "${fsckEnv}:/run/consul-template-wrapper/env"
+  '';
+in
+  lib.buildImageWithPerps {
+    name = "gremlin/fsck";
+    fromImage = lib.images.kubernetesBaseImage;
+    services = [
+      {
+        name = "gremlin-fsck";
+        preStartScript = fsckPreStart;
+        environmentFile = "/run/consul-template-wrapper/env";
+        command = "${contrail32Cw.tools.contrailApiCliWithExtra}/bin/contrail-api-cli fsck";
+        fluentd = {
+          source = {
+            type = "stdout";
+            format = "json";
+          };
+        };
+      }
+    ];
+  }
